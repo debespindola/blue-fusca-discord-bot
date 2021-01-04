ccbb10f5033c20ec6b655dd1a7131a42f9cb052a
Deborah Espíndola <deo2@cin.ufpe.br>
2021-01-04 14:56:17 -0300

    make fusca appear in a loop

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import os
 import discord 
 import random
+import datetime
+import asyncio
 
 from dotenv import load_dotenv 
-from discord.ext import commands
+from discord.ext import commands, tasks
+from randomtimestamp import randomtimestamp
 
 from gifs import gifs
+
+#================================================
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,8 +18,19 @@ GUILD= os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='/')
 
+#=================================================
+
 @bot.command()
 async def tapa(ctx, arg):
     await ctx.send(f'tapa em {arg} \n{random.choice(gifs)}')
+
+@bot.command()
+async def appear_fusca(ctx):
+    while True:
+        await ctx.send(f'FUSCA AZUL PASSANDOOO!!! https://media3.giphy.com/media/56zX8No3gs1e72gflB/giphy.gif')
+        await asyncio.sleep(10)
+
+
+#==================================================
 
 bot.run(TOKEN)
